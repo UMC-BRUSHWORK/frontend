68756a4f6824d135760078bdaca106ffe5f983ee
hanjuuuuuu <hanjucomss@naver.com>
2024-01-08 03:22:40 +0900

    feat: 작성 완료 버튼 컴포넌트 생성

---
+++ src/styles/color.js
@@ -0,0 +1,8 @@
+const color = {
+  white: '#FFFFFF',
+  primary_light: '#D4C4ED',
+  primary_dark: '#4812A3',
+  gradation_dark: 'linear-gradient(225deg, #8C4FF2 0%, #4812A3 100%)',
+};
+
+export default color;
