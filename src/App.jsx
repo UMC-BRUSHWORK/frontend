53ff014283cce168967340a1cac3f4814a3874ef
marchfirst01 <marchfirst3113@gmail.com>
2024-01-10 23:33:35 +0900

    feat: 작품 리스트 컴포넌트 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import StackGrid from 'react-stack-grid';
+import ArtWork from './Components/Common/ArtWork/ArtWork';
+import dummy from './constants/dummy';
 
 function App() {
-  return <div />
+  console.log(window.innerWidth);
+  return (
+    <>
+      <p>asdf</p>
+      <StackGrid
+        columnWidth={200}
+        // columnWidth={(window.innerWidth - 20) / 2}
+        gutterWidth={14}
+        gutterHeight={12}
+      >
+        {dummy.map((item) => (
+          <div key={item.id}>
+            <ArtWork artName={item.artname} artist={item.artist} />
+          </div>
+        ))}
+      </StackGrid>
+    </>
+  );
 }
 
 export default App;
---
+++ src/constants/dummy.js
@@ -0,0 +1,11 @@
+const dummy = [
+  { id: 1, artname: '작품1', artist: '작가1' },
+  { id: 2, artname: '작품2', artist: '작가2' },
+  { id: 3, artname: '작품3', artist: '작가3' },
+  { id: 4, artname: '작품4', artist: '작가4' },
+  { id: 5, artname: '작품5', artist: '작가5' },
+  { id: 6, artname: '작품6', artist: '작가6' },
+  { id: 7, artname: '작품7', artist: '작가7' },
+];
+
+export default dummy;
