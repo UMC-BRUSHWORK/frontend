a0c0b8ae2bd5d40cbca8d38b0bf80177d8fc44ba
EunSo0 <dmstn423@naver.com>
2024-01-12 15:20:36 +0900

    feat: 개인정보 수정 모달 생성

--- src/styles/font.js
+++ src/styles/font.js
@@ -8,7 +8,9 @@ function FONT({ weight, size }) {
 
 const font = {
   // 10px = 1rem
-  medium_17: FONT({ weight: 500, size: 1.7 }), // 17px
+  medium_13: FONT({ weight: 500, size: 1.3 }), // 17px
+  medium_14: FONT({ weight: 500, size: 1.4 }),
+  medium_17: FONT({ weight: 500, size: 1.7 }),
 };
 
 export default font;
