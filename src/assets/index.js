b5c30065f9b1dbc82c84a2e0922707717f1443f6
marchfirst01 <marchfirst3113@gmail.com>
2024-01-14 14:11:27 +0900

    chore: 이미지 더미 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,21 @@
 import profile from './profile.png';
-import artWork from './art_work.png';
+import artWork1 from './art_works/art_work_1.png';
+import artWork2 from './art_works/art_work_2.png';
+import artWork3 from './art_works/art_work_3.png';
+import artWork4 from './art_works/art_work_4.png';
+import artWork5 from './art_works/art_work_5.png';
+import artWork6 from './art_works/art_work_6.png';
+import artWork7 from './art_works/art_work_7.png';
 
 const IMAGES = {
   profile,
-  artWork,
+  artWork1,
+  artWork2,
+  artWork3,
+  artWork4,
+  artWork5,
+  artWork6,
+  artWork7,
 };
 
 export default IMAGES;
