7509f58e3a76ca6eaad7f2a4d973dd1220f3e8bf
marchfirst01 <marchfirst3113@gmail.com>
2024-01-15 23:42:44 +0900

    feat: Dropdown 컴포넌트 디자인

---
+++ src/components/artist/dropdown/Dropdown.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import IMAGES from '../../../assets';
+import * as S from './Dropdown.style';
+import useDetectClose from './useDetectClose';
+
+function DropDown({ handleClick }) {
+  const [myPageIsOpen, myPageRef, myPageHandler] = useDetectClose(false);
+  const [selected, setSelected] = useState('전체보기');
+
+  const handleAllClick = () => {
+    handleClick('all');
+    setSelected('전체보기');
+  };
+
+  const handleSaleClick = () => {
+    handleClick('sale');
+    setSelected('판매중');
+  };
+
+  const handleSoldOutClick = () => {
+    handleClick('soldOut');
+    setSelected('판매 완료');
+  };
+
+  return (
+    <S.Container>
+      <S.DropDownBtn
+        onClick={myPageHandler}
+        ref={myPageRef}
+        $isDropped={myPageIsOpen}
+      >
+        <p>{selected}</p>
+        <img src={IMAGES.arrowDropDown} alt="arrow" />
+      </S.DropDownBtn>
+      <S.Menu $isDropped={myPageIsOpen}>
+        <S.Ul>
+          <S.Li onClick={handleAllClick}>
+            <p id="name">전체보기</p>
+          </S.Li>
+          <S.Li onClick={handleSaleClick}>
+            <p id="newest">판매중</p>
+          </S.Li>
+          <S.Li onClick={handleSoldOutClick}>
+            <p id="newest">판매 완료</p>
+          </S.Li>
+        </S.Ul>
+      </S.Menu>
+    </S.Container>
+  );
+}
+
+export default DropDown;
---
+++ src/components/artist/dropdown/Dropdown.style.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+import font from '../../../styles/font';
+
+export const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 3rem;
+
+  cursor: pointer;
+`;
+
+export const DropDownBtn = styled.div`
+  position: absolute;
+  right: 0px;
+
+  display: flex;
+  flex-direction: row;
+
+  width: 7.9rem;
+  padding: 8px 12px;
+  justify-content: center;
+  align-items: center;
+
+  ${() => font.regular_10}
+`;
+
+export const Menu = styled.div`
+  z-index: 1;
+  position: absolute;
+  top: 3rem;
+  right: 0;
+
+  width: 7.9rem;
+
+  border-radius: 0px 0px 5px 5px;
+  background: var(--Grayscale-White, #fff);
+  box-shadow: 0px 1px 1.5px 0px rgba(0, 0, 0, 0.15);
+
+  color: ${(props) => props.theme['grayscale-40']};
+  box-shadow: ${(props) => props.theme['shadow-1pt']};
+
+  visibility: hidden;
+  opacity: 0;
+  transform: translateY(-10px); /* 수정: 위쪽으로 이동하는 값 설정 */
+  transition:
+    opacity 0.2s ease,
+    transform 0.2s ease,
+    visibility 0.2s;
+
+  &:after {
+    content: '';
+    height: 0;
+    width: 0;
+    position: absolute;
+    transform: translate(0, 0);
+    border: 12px solid transparent;
+  }
+
+  ${({ $isDropped }) =>
+    $isDropped &&
+    `
+      opacity: 1;
+      visibility: visible;
+      transform: translateY(0); /* 수정: 열릴 때는 원래 위치로 이동 */
+    `};
+`;
+
+export const Ul = styled.div`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Li = styled.div`
+  padding: 6px 16px;
+
+  color: ${(props) =>
+    props.$active ? props.theme['blue-50'] : props.theme['grayscale-50']};
+
+  &:hover {
+    font-weight: 600;
+  }
+`;
