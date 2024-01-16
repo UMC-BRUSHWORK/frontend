3cb6e9456fd13e227cccadf85864f7fcdce0f10f
marchfirst01 <marchfirst3113@gmail.com>
2024-01-16 16:21:27 +0900

    style: Artist 페이지 수정

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Temperature from '../components/artist/temperature/Temperature';
 
 export default function Artist() {
-  return <Temperature />;
+  return <div />;
 }
