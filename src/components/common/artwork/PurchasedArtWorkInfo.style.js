2e3322e0ad24068a83adadfbe3f0b6416ae64fb7
hanjuuuuuu <hanjucomss@naver.com>
2024-01-17 00:23:12 +0900

    fix: 파일 이름 변경

--- src/components/common/artwork/PurchasedArtWorkInfo.style.js
+++ src/components/common/artwork/PurchasedArtWorkInfo.style.js
@@ -2,36 +2,36 @@ import styled from 'styled-components';
 import font from '../../../styles/font';
 import color from '../../../styles/color';
 
-export const ArtInfoWrapper = styled.div`
+export const PurchasedArtInfoWrapper = styled.div`
   display: flex;
   margin-top: 1.7rem;
 `;
 
-export const ArtworkImage = styled.img`
+export const PurchasedArtworkImage = styled.img`
   border-radius: 5px;
   width: 8.8rem;
   height: 8.8rem;
 `;
 
-export const ArtWrapper = styled.div`
+export const PurchasedArtWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 1.2rem;
 `;
 
-export const ArtName = styled.h1`
+export const PurchasedArtName = styled.h1`
   color: ${color.black};
   ${() => font.medium_13}
   margin-top: 0.4rem;
 `;
 
-export const ArtistName = styled.p`
+export const PurchasedArtistName = styled.p`
   color: ${color.gray};
   ${() => font.regular_10}
   margin-top: 0.2rem;
 `;
 
-export const ArtPrice = styled.p`
+export const PurchasedArtPrice = styled.p`
   color: ${color.black};
   ${() => font.regular_12}
   margin-top: 0.2rem;
