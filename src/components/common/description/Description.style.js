b9fc12864fccccedd8ae9f56043c2e8653a84c85
techncherry <technarchitect@gmail.com>
2024-01-19 20:13:26 +0900

    feat: 상세설명 컴포넌트 수정

--- src/components/common/description/Description.style.js
+++ src/components/common/description/Description.style.js
@@ -1,23 +1,30 @@
 import styled from "styled-components";
 
 export const Title = styled.div`
-    font-size: 13px;
+    font-size: 1.3rem;
+    font-style: normal;
     font-weight: 500;
+    line-height: normal;
     margin-top: 1rem;
     margin-bottom: 1rem;
+    color: ${props => props.active ? '#4812A3' : '#000000'}
 `
 
 export const TextArea = styled.textarea`
-    border: 1px solid #BEBEBE;
-    border-radius: 3px;
+    border: 1px solid var(--Grayscale-BE, #BEBEBE);
+    border-radius: 0.6rem;
     width: 100%;
     height: 13rem;
-    font-size: 17px;
-    font-weight: 600;
+    color: var(--Grayscale-33, #333);
+    text-align: justify;
+    font-size: 1.7rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
     padding: 1rem;
     &::placeholder{
 		color: #BEBEBE;
 	}
-    outline-color: #8C4FF2;
-    outline-width: 1px;
+    outline-color: var(--Primary_normal, #8C4FF2);
+    outline-width: 0.1rem;
 `
