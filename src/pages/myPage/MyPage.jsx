40eb862b6246431cbd495b606f90e01bcd4c8e1b
EunSo0 <dmstn423@naver.com>
2024-01-22 11:42:47 +0900

    fix: myPage 폴더 추가

--- src/pages/myPage/MyPage.jsx
+++ src/pages/myPage/MyPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from '../components/common/header/Header';
-import BottomNav from '../components/common/bottomNav/BottomNav';
-import LogoutStatus from '../components/mypage/LogoutStatus';
+import Header from '../../components/common/header/Header';
+import BottomNav from '../../components/common/bottomNav/BottomNav';
+import LogoutStatus from '../../components/mypage/LogoutStatus';
 
 const Wrapper = styled.div`
   height: calc(100vh - 11.2rem);
