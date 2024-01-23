83470579a3910f679138ff645685dfc600869cc0
marchfirst01 <marchfirst3113@gmail.com>
2024-01-23 15:39:46 +0900

    fix: artwork size 변수 수정

--- src/components/common/artwork/ArtWork.jsx
+++ src/components/common/artwork/ArtWork.jsx
@@ -7,6 +7,7 @@ export default function ArtWork({
   artName = '작품 이름',
   artist = '작가명',
   direction = 'row',
+  size = '12rem',
 }) {
   const imgSrc = IMAGES[artSrc];
   const [favorite, setFavorite] = useState('off');
