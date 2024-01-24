dc40a48a15181e7e51e55f571ec666d5e54fed20
techncherry <technarchitect@gmail.com>
2024-01-24 20:37:01 +0900

    feat: 작품 등록 페이지 Topbar 추가

--- src/components/common/topbar/Topbar.style.js
+++ src/components/common/topbar/Topbar.style.js
@@ -1,27 +1,37 @@
 import styled from 'styled-components';
+import font from '../../../styles/font';
 
 export const Wrapper = styled.div`
-    width: 100%;
-    height: 5.2rem;
-    padding-left: 2rem;
-    padding-right: 2rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  width: 100%;
+  height: 5.2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Left = styled.div`
-    display: flex;
+  display: flex;
+`;
+
+export const LogoText = styled.div`
+  ${() => font.medium_19};
+  margin-left: 1.2rem;
 `;
 
 export const Right = styled.div`
-    display: flex;
-    justify-content: flex-start;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const IconWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
 `;
 
 export const Icon = styled.img`
-    width: 2.4rem;
-    height: 2.4rem;
-    flex-shrink: 0;
+  width: 2.4rem;
+  height: 2.4rem;
+  flex-shrink: 0;
 `;
